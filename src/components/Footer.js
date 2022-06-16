7afca5a2848ab783407032e129c9809fc070eb2b
Indykatz <cats83@me.com>
2022-06-16 13:14:08 +0100

    Olly Footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,65 @@
-import { Footer } from "../css/Footer.styled";
+import {
+  Footer,
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinksItems,
+  FooterLinkTitle,
+  FooterLink,
+} from "../css/Footer.styled";
 
 const FooterBanner = ({ name, github }) => {
-  return <Footer>Kat | Olly | Abdelbar</Footer>;
+  return (
+    <div>
+      <Footer>Kat | Olly | Abdelbar</Footer>
+      <FooterContainer>
+        <FooterWrap>
+          <FooterLinksContainer>
+            <FooterLinksWrapper>
+              <FooterLinksItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+
+                <FooterLink to="/About">Testimonials</FooterLink>
+                <FooterLink to="/About">Careers</FooterLink>
+                <FooterLink to="/About">Investors</FooterLink>
+                <FooterLink to="/About">About</FooterLink>
+              </FooterLinksItems>
+
+              <FooterLinksItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+
+                <FooterLink to="/About">Testimonials</FooterLink>
+                <FooterLink to="/About">Careers</FooterLink>
+                <FooterLink to="/About">Investors</FooterLink>
+                <FooterLink to="/About">About</FooterLink>
+              </FooterLinksItems>
+            </FooterLinksWrapper>
+
+            <FooterLinksWrapper>
+              <FooterLinksItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+
+                <FooterLink to="/About">Testimonials</FooterLink>
+                <FooterLink to="/About">Careers</FooterLink>
+                <FooterLink to="/About">Investors</FooterLink>
+                <FooterLink to="/About">About</FooterLink>
+              </FooterLinksItems>
+
+              <FooterLinksItems>
+                <FooterLinkTitle>About Us</FooterLinkTitle>
+
+                <FooterLink to="/About">Testimonials</FooterLink>
+                <FooterLink to="/About">Careers</FooterLink>
+                <FooterLink to="/About">Investors</FooterLink>
+                <FooterLink to="/About">About</FooterLink>
+              </FooterLinksItems>
+            </FooterLinksWrapper>
+          </FooterLinksContainer>
+        </FooterWrap>
+      </FooterContainer>
+    </div>
+  );
 };
 
 export default FooterBanner;
--- src/css/Footer.styled.js
+++ src/css/Footer.styled.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Footer = styled.div`
   bottom: 0px;
@@ -8,12 +9,69 @@ export const Footer = styled.div`
   color: #000407;
   width: 100%;
 `;
-export const FooterLinkItem = styled.a`
-  font-size: 16px;
-  margin-right: 16px;
-  color: #000407;
+
+export const FooterContainer = styled.footer`
+  background-color: #2f3061;
+  bottom: 0px;
+`;
+
+export const FooterWrap = styled.div`
+  padding: 48px 24px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+export const FooterLinksContainer = styled.div`
+  display: flex;
+  justify-content: center;
+
+  @media screen and (max-width: 820px) {
+    padding-tio: 32px;
+  }
+`;
+
+export const FooterLinksWrapper = styled.div`
+  display: flex;
+
+  @media screen and (max-width: 820px) {
+    flex-direction: table-column;
+  }
+`;
+
+export const FooterLinksItems = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  margin: 16px;
+  text-align: left;
+  width: 160px;
+  box-sizing: border-box;
+  color: #bdefbf;
+
+  @media screen and (max-width: 420px) {
+    margin: 0;
+    padding: 10px;
+    width: 100%;
+  }
+`;
+
+export const FooterLinkTitle = styled.h1`
+  font-size: 14px;
+  margin-bottom: 16px;
+`;
+
+export const FooterLink = styled(Link)`
+  color: #9f9493;
+  text-decoration: none;
+  margin-bottom: 0.5rem;
+  font-size: 14px;
+
   &:hover {
-    opacity: 0.5;
-    cursor: pointer;
+    color: #01bf71;
+    transition: 0.3s ease-out;
   }
 `;
